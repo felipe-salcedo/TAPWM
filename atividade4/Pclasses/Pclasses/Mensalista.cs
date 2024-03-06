d93a43100f899ccf9ce4cab47363c47860d7b154
FELIPE SALCEDO RAMALHO <0031432412008@lti.fatecso.edu.br>
2024-03-06 18:00:01 -0300

    subindo Pclass

---
+++ atividade4/Pclasses/Pclasses/Mensalista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pclasses
+{
+    class Mensalista : Empregado
+    {
+        public double SalarioMensal { get; set; }
+
+        public static String Empresa = "Irmãos Silva ltda.";
+    
+        public override double SalarioBruto()
+        { 
+            return SalarioMensal;
+        }
+    }
+
+}
---
+++ atividade4/Pclasses/Pclasses/frmMensalista.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Pclasses
+{
+    partial class frmMensalista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.btnInstancia1 = new System.Windows.Forms.Button();
+            this.lblMatricula = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnInstancia2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(349, 120);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 26);
+            this.txtSalario.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(349, 88);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(275, 26);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(349, 56);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(73, 26);
+            this.txtMatricula.TabIndex = 0;
+            // 
+            // btnInstancia1
+            // 
+            this.btnInstancia1.Location = new System.Drawing.Point(120, 245);
+            this.btnInstancia1.Name = "btnInstancia1";
+            this.btnInstancia1.Size = new System.Drawing.Size(191, 87);
+            this.btnInstancia1.TabIndex = 1;
+            this.btnInstancia1.Text = "Instanciar Matrícula";
+            this.btnInstancia1.UseVisualStyleBackColor = true;
+            // 
+            // lblMatricula
+            // 
+            this.lblMatricula.AutoSize = true;
+            this.lblMatricula.Location = new System.Drawing.Point(116, 56);
+            this.lblMatricula.Name = "lblMatricula";
+            this.lblMatricula.Size = new System.Drawing.Size(73, 20);
+            this.lblMatricula.TabIndex = 3;
+            this.lblMatricula.Text = "Matrícula";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(116, 88);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(51, 20);
+            this.lblNome.TabIndex = 4;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(116, 120);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(113, 20);
+            this.lblSalario.TabIndex = 5;
+            this.lblSalario.Text = "Salário Mensal";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(116, 152);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(195, 20);
+            this.lblData.TabIndex = 7;
+            this.lblData.Text = "Data Entrada na Empresa";
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(349, 152);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(100, 26);
+            this.txtData.TabIndex = 6;
+            // 
+            // btnInstancia2
+            // 
+            this.btnInstancia2.Location = new System.Drawing.Point(457, 245);
+            this.btnInstancia2.Name = "btnInstancia2";
+            this.btnInstancia2.Size = new System.Drawing.Size(191, 87);
+            this.btnInstancia2.TabIndex = 8;
+            this.btnInstancia2.Text = "Instanciar Mensalista passando parâmetros";
+            this.btnInstancia2.UseVisualStyleBackColor = true;
+            // 
+            // frmMensalista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInstancia2);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblMatricula);
+            this.Controls.Add(this.btnInstancia1);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtSalario);
+            this.Name = "frmMensalista";
+            this.Text = "FrmMensalista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.Button btnInstancia1;
+        private System.Windows.Forms.Label lblMatricula;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnInstancia2;
+    }
+}
